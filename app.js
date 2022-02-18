a586ff0467ffe29367d984e385c294eb72974fc3
Garvit Gupta <garvitg02@gmail.com>
2022-02-18 15:21:01 +0530

    first draft of weather api

---
+++ app.js
@@ -0,0 +1,37 @@
+const express=require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+  const query=req.body.CityName;
+  const apiKey="0212c6f24e50d634f6498bffcc01481f";
+  const unit="metric";
+  const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
+  https.get(url,function(response){
+    console.log(response.statusCode);
+    response.on("data",function(data){
+      const weatherData=JSON.parse(data);
+      const temp=weatherData.main.temp;
+      const weatherDescription=weatherData.weather[0].description;
+      const icon=weatherData.weather[0].icon;
+      const imgurl="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+      res.write("<p>The currently temp in "+query+" is "+weatherDescription+"</p>");
+      res.write("<h1>The temp in "+query+" is "+temp+" degree Celcius. </h1>");
+      res.write("<img src="+imgurl+ ">");
+      res.send();
+    });
+  });
+});
+
+app.listen(3000,function(){
+  console.log("Server is up and running on port 3000");
+})
